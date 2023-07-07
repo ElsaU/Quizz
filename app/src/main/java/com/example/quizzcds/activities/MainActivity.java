6cc0da23270e9679878fa5139fc45f405d0a94c0
ElsaU <elsaucha@gmail.com>
2023-07-07 11:27:46 +0200

    QuizzCDS app

---
+++ app/src/main/java/com/example/quizzcds/activities/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.quizzcds.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.quizzcds.R;
+
+public class MainActivity extends AppCompatActivity {
+    Button buttonStart;
+    Button buttonRanking;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonRanking = findViewById(R.id.buttonRanking);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, QuizzActivity.class));
+            }
+        });
+
+        buttonRanking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(MainActivity.this, QuizzActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/quizzcds/activities/QuizzActivity.java
@@ -0,0 +1,16 @@
+package com.example.quizzcds.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quizzcds.R;
+
+public class QuizzActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizz);
+    }
+}
